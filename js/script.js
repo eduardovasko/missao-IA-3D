bbced5ba0c7c7d6b1337fbf78981a10d7680bac4
eduardovasko <130376753+eduardovasko@users.noreply.github.com>
2024-09-11 08:45:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".novamente-btn");
 
 
 
@@ -44,6 +44,15 @@ function mostraResultado(){
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = ""; 
+    caixaResultado.classlist.add("mostrar");
+    botaoJogarNovamente.addEventListener("click"; jogarNovamente);
+}
+function jogarNovamente {
+    atua = 0;
+    historiaFinal = "";
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
 }
 
+    
 mostraPergunta();
